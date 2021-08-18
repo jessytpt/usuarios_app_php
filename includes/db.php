9f593ef34877ba68810017cd110d1b7eb75e2a7a
Jessenia Toapanta <jessytpt.dev@gmail.com>
2021-08-18 11:20:57 -0500

    agregando cambios

---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'usuarios'
+);
+if(isset($conn)){
+ //   echo 'DB is connect';
+}
+
+?>
---
+++ includes/index.php
@@ -0,0 +1,45 @@
+<?php include("db.php") ?>
+
+<?php include("header.php") ?>
+
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4">
+        <!-- MESSAGES -->
+
+        <?php if (isset($_SESSION['message'])) { ?>
+      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+        <?= $_SESSION['message']?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <?php session_unset(); } ?>
+
+      <!-- ADD TASK FORM -->             
+
+
+
+                <div class="car car-body">
+                    <form action="save.php" method="POST">
+                        <div class="form-group">
+                            <input type="text" name="title" class="form-control"
+                            placeholder="Task title">
+                        </div>
+                        <div class="form-group">
+                            <textarea name="description" rows="2" class="form-control"
+                            placeholder="Task description"></textarea>
+                        </div>
+                        <input type="submit" class="btn btn-success btn-block" 
+                        name="save_task" value="Save Task">
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-8"></div>
+        </div>
+
+    </div>
+
+  
+
+<?php include("footer.php") ?>
